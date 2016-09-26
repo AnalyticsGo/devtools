1b51bb94b1623d2cab82492c342211fdacbcdc76
Minh Chau <nmchau87@gmail.com>
2016-09-26 23:48:36 +0900

    Add ch-keyword extraction

--- clickhouse-functions-parser/clickhouse-functions-parser.js
+++ clickhouse-functions-parser/clickhouse-functions-parser.js
@@ -1,10 +1,31 @@
 var fs = require('fs')
 var cheerio = require('cheerio')
+ var keywordsList = new Set()
+var functionsList = new Set()
 
 var ch_parser = function(html) {
-  var functionsList = new Set()
   var $ = cheerio.load(html)
   var contents = $('#contents')
+
+  var keywordSection = contents.find('a:contains("Query language")')
+  var currentTag = keywordSection.first().next()
+  do {
+    var textTag = currentTag.text()
+    if (textTag.length > 0) {
+      var keywordElement = null
+      if (/[A-Z]{2,}/.test(textTag)) {
+        // pattern XX XX yyyy
+        keywordElement = textTag.match(/[A-Z]{2,}/g)
+      }
+      if (keywordElement != null) {
+        keywordElement.forEach(function(name) {
+          keywordsList.add(name.trim())
+        })
+      }
+    }
+    currentTag = currentTag.next()
+  } while (currentTag.text() !== "Table engines")
+
   var functionSection = contents.find('a:contains("Functions")')
   var currentTag = functionSection.eq(1).next()
 
@@ -76,21 +97,22 @@ var ch_parser = function(html) {
     }
     currentTag = currentTag.next()
   } while (currentTag.text() !== "Dictionaries")
-
-  return functionsList
 }
 
 if (process.argv.length < 4) {
-  console.log('Usage: node clickhouse-functions-parser.js input_html_content output_js_functions')
+  console.log('Usage: node clickhouse-functions-parser.js input_ch_html output_ch_js')
   process.exit(1)
 }
 
 fs.readFile(process.argv[2], 'utf8', function(err, data) {
   if (err) throw err
-  var functionsList = ch_parser(data)
+  ch_parser(data)
   var file = fs.createWriteStream(process.argv[3])
-  file.write("var clickhouseFunctionsList = [ \n")
-  file.write('\t' + Array.from(functionsList).join(',\n\t') + '\n')
+  file.write("var chKeywordsList = [ \n")
+  file.write('\t"' + Array.from(keywordsList).join('",\n\t"') + '"\n')
+  file.write("]\n")
+  file.write("var chFunctionsList = [ \n")
+  file.write('\t"' + Array.from(functionsList).join('",\n\t"') + '"\n')
   file.write("]")
   file.end()
 });
